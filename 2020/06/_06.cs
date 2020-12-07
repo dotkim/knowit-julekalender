367ccf3a1b7d8ee67855f058bec50be0532b9786
Kim Nerli <kim-andre-skole@hotmail.com>
2020-12-07 02:48:08 +0100

    Unchecked solution

--- 2020/06/_06.cs
+++ 2020/06/_06.cs
@@ -7,17 +7,19 @@ namespace Knowit
 {
   public static class _06
   {
-    private static string _Input = FileHelper.GetInput(@"06\input");
+    private static string _Input = FileHelper.GetInput(@"06/input");
 
     public static void Run()
     {
       int sum = 0;
+      int max = 0;
       foreach (int pack in Array.ConvertAll(_Input.Split(","), int.Parse))
       {
+        if ((sum / 127.0) % 1 == 0) max = Math.Max(max, (sum / 127));
         sum += pack;
       }
 
-      Console.WriteLine($"Luke 6:\t{sum / 127.0}");
+      Console.WriteLine($"Luke 6:\t{max}");
     }
   }
 }
